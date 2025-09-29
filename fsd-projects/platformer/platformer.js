39bd52e3f523e74fedc84e6eaadd5a22a9470e10
DiamondBri <diamond.peters@inspire35.org>
2025-09-29 19:13:30 +0000

    qawe

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,23 +27,38 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
+createPlatform(120,700,150,20,"pink");
+createPlatform(250,640,130,20,"black");
+createPlatform(350,520,100,20,"pink");
+createPlatform(420,400,100,20,"black");
+createPlatform(500,570,150,20,"pink");
+createPlatform(400,200,120,20,"black");
+createPlatform(550, 150, 100, 20, "gray");
+createPlatform(550,340,130,20,"black");
+createPlatform(720,220,100,20,"pink");
+createPlatform(750,600,100,20,"black");
+createPlatform(820,450,150,20,"pink");
+createPlatform(980,540,120,20,"black");
+createPlatform(1000,700, 100, 20, "gray");
 
 
     // TODO 3 - Create Collectables
-
+createCollectable("steve", 450, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
+createCollectable("grace", 300, 100,0.2);
 
 
     
     // TODO 4 - Create Cannons
-
-
-    
+createCannon("right",450,900);
+createCannon("right",150,900);
+createCannon("top",250,750);
+createCannon("top",900,900);
+ 
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
